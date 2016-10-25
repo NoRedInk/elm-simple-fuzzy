59368326b27f3c6b41aecdb486f385dc54065a77
Aaron Strick <aaron@noredink.com>
2016-10-25 09:53:19 -0700

    initial commit

---
+++ src/String/Filter.elm
@@ -0,0 +1,13 @@
+module String.Filter exposing (search)
+
+{-| # String.Filter
+
+Fuzzy match through a list of strings
+
+
+-}
+
+
+search : String
+search =
+    "helloworld"
---
+++ tests/Tests.elm
@@ -0,0 +1,10 @@
+module Tests exposing (..)
+
+import Test exposing (..)
+import Expect exposing (Expectation)
+import String.Filter
+
+
+all : Test
+all =
+    describe "String.Filter" []
